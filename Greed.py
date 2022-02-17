babde3c330f11d83246f69e44bff3b1d02243e2e
DavidCockbain <98206594+DavidCockbain@users.noreply.github.com>
2022-02-17 22:57:12 +0200

    Update Greed.py

--- Greed.py
+++ Greed.py
@@ -1,3 +1,22 @@
 print("Greed")
-print("hellow")
-print('hey')
+import random
+
+print("Greed")
+
+class Falling_row:
+    def generate_row():
+        gem_row = [' '] * 100
+        gem = random.randint(0,99)
+        rock = random.randint(0,99)
+        gem_row[gem] = '*'
+        gem_row[rock] = 'O'
+        return gem_row
+    
+    def print_row():
+        gem_row = Falling_row.generate_row()
+        for character in gem_row:
+            print(character, end = '')
+
+for i in range(15):
+    Falling_row.print_row()
+
